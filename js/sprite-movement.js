8b5994241881c5537f196bed65da4daca23838c4
Michael <numtrixteam@gmail.com>
2025-08-07 20:09:26 -0500

    respawn

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -13,4 +13,21 @@ if(platform.y > 410)
 
 if(platform2.y > 410)
     {platform2.y = -20;}
+
+
+    // Reset player when it falls off screen
+    if (player.y > 400) {
+        player.y = 200; // Reset to middle of screen
+        player.x = 200; // Reset to middle of screen
+    }
+    
+    // Your existing star respawn code...
+    if (star.y > 400) {
+        star.y = 0;
+        star.x = randomNumber(0, 400);
+    }
+    if (star2.y > 400) {
+        star2.y = 0;
+        star2.x = randomNumber(0, 400);
+    }
 }
--- js/sprite-setup.js
+++ js/sprite-setup.js
@@ -6,7 +6,7 @@ function setupSprites() {
   player.addAnimation('fly', alienAnimation);
   player.height = 50;
   player.width = 50;
-  player.velocityY =1;
+  player.velocityY =0;
 
   star = createSprite(100, 0, 50, 50);
   star.shapeColor = "aqua";
