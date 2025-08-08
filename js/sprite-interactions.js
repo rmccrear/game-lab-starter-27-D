e6586ec36cdf8d8426847a831966a79a5d146a48
Michael <numtrixteam@gmail.com>
2025-08-07 19:48:26 -0500

    starfix

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -9,10 +9,12 @@ function doSpriteInteraction() {
         star.y = 0; // Reset to top
         star.x = randomNumber(0, 400); // Random x position
 
-        if (player.isTouching(star2)) {
+        
+    }
+ if (player.isTouching(star2)) {
         score += 1;
         star2.y = 0; // Reset to top  
         star2.x = randomNumber(0, 400); // Random x position
-    }
+
     }
 }
