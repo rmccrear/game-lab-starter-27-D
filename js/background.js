2d966a89c19b21b4eb71a11c2a53fb1c79a4ee54
Michael <numtrixteam@gmail.com>
2025-08-07 20:42:52 -0500

    background

--- js/background.js
+++ js/background.js
@@ -1,21 +1,22 @@
 //BACKGROUND CODE
 function drawBackground() {
+    // Check score and change background color
+    if (score >= 10) {
+        background('orange');
+       
+    
+    } else {
+        background('darkblue');
+    }
     background1();
 }
 
 function background1() {
-    background("darkBlue");
     noStroke();
-    fill(255, 255, 0); //Yellow
+    fill(255, 255, 0); //yellow
     ellipse(randomNumber(0, 400), randomNumber(0, 400), 3, 3);
     ellipse(randomNumber(0, 400), randomNumber(0, 400), 3, 3);
     ellipse(340, 50, 60, 60);
     fill(20, 20, 150); //darkblue
     ellipse(320, 30, 60, 60);
-  }
-
-  rect(200, 200, 10, 2,)
-
-  function background2() {
-     ellipse(320, 30, 60, 60);
-  }
+}
